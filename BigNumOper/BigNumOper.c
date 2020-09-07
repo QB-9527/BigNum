e76bf50054e2310cae63760b974a0aff4b581c17
QB-9527 <wengk99@gmail.com>
2020-09-07 17:16:19 +0800

    微調-2

--- BigNumOper/BigNumOper.c
+++ BigNumOper/BigNumOper.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #define MAX 50
 
-void addOrSub(int xNum1[], int xNum2[], int xSub, int xLen, int xNeg) {
+void doAdditionOrSubtraction(int xNum1[], int xNum2[], int xSub, int xLen, int xNeg) {
     //單純做純數字相加或相減，符號僅以xNeg做判斷
     int i;
 
@@ -48,7 +48,7 @@ void addOrSub(int xNum1[], int xNum2[], int xSub, int xLen, int xNeg) {
 
 }
 
-void mul(int xNum1[], int xNum2[], int xNum1Len, int xNum2Len, int xNeg) {
+void doMultiplication(int xNum1[], int xNum2[], int xNum1Len, int xNum2Len, int xNeg) {
     int i, j;
     int mLen = xNum1Len + xNum2Len;
     int* mAns = calloc(mLen, sizeof(int));
@@ -75,9 +75,6 @@ void mul(int xNum1[], int xNum2[], int xNum1Len, int xNum2Len, int xNeg) {
     if (xNeg == 1) {
         printf("-");
     }
-    /*else {
-        printf("+");
-    }*/
 
     for (i = mLen - 1; i >= 0; i--) {
         printf("%d", mAns[i]);
@@ -88,6 +85,9 @@ void mul(int xNum1[], int xNum2[], int xNum1Len, int xNum2Len, int xNeg) {
 }
 
 void testOper(char xNum1[], char xNum2[], char xOper) {
+	//因減法只考慮大數減小數
+	//在這裡會先確認傳入的第一個數字是否較大，沒有的話做交換並做flag，再做運算
+	
     int i, j;
     int mSwap = 0;      //前後交換flag
     int mNum1Neg = 0;   //Num1負號flag
@@ -152,34 +152,34 @@ void testOper(char xNum1[], char xNum2[], char xOper) {
 
     if (xOper == '+') {
         if (mNum1Neg == 0 && mNum2Neg == 0) {
-            addOrSub(mNum1, mNum2, 0, mLen, 0);
+            doAdditionOrSubtraction(mNum1, mNum2, 0, mLen, 0);
         }
         else if (mNum1Neg == 0 && mNum2Neg == 1) {
-            addOrSub(mNum1, mNum2, 1, mLen, 0);
+            doAdditionOrSubtraction(mNum1, mNum2, 1, mLen, 0);
         }
         else if (mNum1Neg == 1 && mNum2Neg == 0) {
-            addOrSub(mNum1, mNum2, 1, mLen, 1);
+            doAdditionOrSubtraction(mNum1, mNum2, 1, mLen, 1);
         }
         else if (mNum1Neg == 1 && mNum2Neg == 1) {
-            addOrSub(mNum1, mNum2, 0, mLen, 1);
+            doAdditionOrSubtraction(mNum1, mNum2, 0, mLen, 1);
         }
     }
     else if(xOper == '-'){     //有做過交換的話(mSwap==1),答案也需要加負號
         if (mNum1Neg == 0 && mNum2Neg == 0) {
-            addOrSub(mNum1, mNum2, 1, mLen, 0 ^ mSwap);
+            doAdditionOrSubtraction(mNum1, mNum2, 1, mLen, 0 ^ mSwap);
         }
         else if (mNum1Neg == 0 && mNum2Neg == 1) {
-            addOrSub(mNum1, mNum2, 0, mLen, 0 ^ mSwap);
+            doAdditionOrSubtraction(mNum1, mNum2, 0, mLen, 0 ^ mSwap);
         }
         else if (mNum1Neg == 1 && mNum2Neg == 0) {
-            addOrSub(mNum1, mNum2, 0, mLen, 1 ^ mSwap);
+            doAdditionOrSubtraction(mNum1, mNum2, 0, mLen, 1 ^ mSwap);
         }
         else if (mNum1Neg == 1 && mNum2Neg == 1) {
-            addOrSub(mNum1, mNum2, 1, mLen, 1 ^ mSwap);
+            doAdditionOrSubtraction(mNum1, mNum2, 1, mLen, 1 ^ mSwap);
         }
     }
     else if (xOper == '*') {
-        mul(mNum1, mNum2, mLen, mNum2Len, mNum1Neg ^ mNum2Neg);
+        doMultiplication(mNum1, mNum2, mLen, mNum2Len, mNum1Neg ^ mNum2Neg);
     }
 
     free(mNum1);
@@ -187,39 +187,40 @@ void testOper(char xNum1[], char xNum2[], char xOper) {
 }
 
 int main(){
-    testOper("0", "0", '-');
-    testOper("0", "-2", '-');
-    testOper("-1", "0", '-');
-    testOper("0", "0", '-');
-    testOper("-9", "5", '+');
-    testOper("-1", "0", '-');
-    testOper("1", "-1", '-');
-    testOper("-1", "9", '-');
-    testOper("-99998", "99999", '-');
+    testOper("0", "0", '-');            printf("+0\n\n");
+    testOper("0", "-2", '-');           printf("+2\n\n");
+    testOper("-1", "0", '-');           printf("-1\n\n");
+    testOper("0", "0", '-');            printf("+0\n\n");
+    testOper("-9", "5", '+');           printf("-4\n\n");
+    testOper("-1", "0", '-');           printf("-1\n\n");
+    testOper("1", "-1", '-');           printf("+2\n\n");
+    testOper("-1", "9", '-');           printf("-10\n\n");
+    testOper("10", "9", '-');           printf("+1\n\n");
+    testOper("-99998", "99999", '-');   printf("-199997\n\n");
 
-    testOper("9999999", "987", '+');
-    testOper("-9999999", "-987", '+');
-    testOper("-9999999", "987", '-');
-    testOper("9999999", "-987", '-');
-    testOper("1111333", "333", '+');
-    testOper("9999999", "987", '-');
-    testOper("-9999999", "-987", '-');
-    testOper("333", "1111333", '+');
-    testOper("333", "-1111333", '+');
-    testOper("-333", "1111333", '+');
-    testOper("333", "1111333", '-');
-    testOper("99999", "99998", '-');
+    testOper("9999999", "987", '+');    printf("+10000986\n\n");
+    testOper("-9999999", "-987", '+');  printf("-10000986\n\n");
+    testOper("-9999999", "987", '-');   printf("-10000986\n\n");
+    testOper("9999999", "-987", '-');   printf("+10000986\n\n");
+    testOper("1111333", "333", '+');    printf("+1111666\n\n");
+    testOper("9999999", "987", '-');    printf("+9999012\n\n");
+    testOper("-9999999", "-987", '-');  printf("-9999012\n\n");
+    testOper("333", "1111333", '+');    printf("+1111666\n\n");
+    testOper("333", "-1111333", '+');   printf("-1111000\n\n");
+    testOper("-333", "1111333", '+');   printf("+1111000\n\n");
+    testOper("333", "1111333", '-');    printf("-1111000\n\n");
+    testOper("99999", "99998", '-');    printf("+1\n\n");
 	
-    testOper("0", "0", '*');
-    testOper("0", "123", '*');
-    testOper("123", "123", '*');
-    testOper("-123", "123", '*');
-    testOper("123", "-123", '*');
-    testOper("-123", "-123", '*');
-    testOper("1231", "123", '*');
-    testOper("-123", "1231", '*');
-    testOper("-2", "99999999999999999999999", '*');
-    testOper("99999999999999999999999", "-2", '*');
+    testOper("0", "0", '*');            printf("0\n\n");
+    testOper("0", "123", '*');          printf("0\n\n");
+    testOper("123", "123", '*');        printf("15129\n\n");
+    testOper("-123", "123", '*');       printf("-15129\n\n");
+    testOper("123", "-123", '*');       printf("-15129\n\n");
+    testOper("-123", "-123", '*');      printf("15129\n\n");
+    testOper("1231", "123", '*');       printf("151413\n\n");
+    testOper("-123", "1231", '*');      printf("-151413\n\n");
+    testOper("-2", "9999999999999999999999999999", '*');    printf("-19999999999999999999999999998\n\n");
+    testOper("9999999999999999999999999999", "-2", '*');    printf("-19999999999999999999999999998\n\n");
     
     //system("pause");
     return 0;
